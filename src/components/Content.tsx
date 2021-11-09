9e2c8d5d9089592ef16e43fd755477ec5749e54f
Luiz Silva <luizsilva.ee@gmail.com>
2021-11-09 12:15:35 -0300

    Refatora componentes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+interface movieListProps {
+ movies: Array<{
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+ }>
+}
+
+export function Content(props: movieListProps) {
+
+  console.log(props);
+
+  return (
+    <main>
+    <div className="movies-list">
+      {props.movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+  );
 }
